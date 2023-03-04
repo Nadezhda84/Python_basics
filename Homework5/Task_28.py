c8bf6db7d3294214fdf8248e45bdbfa5464b8806
Dusha <nadezdafurman84@gmail.com>
2023-03-04 13:31:06 +0600

    Added solution Task 28

--- Homework5/Task_28.py
+++ Homework5/Task_28.py
@@ -8,3 +8,21 @@
 2 2
     4
 """
+
+from timeit import timeit
+
+
+def sum(a):
+    if a == 1:
+        return 1
+    return 1 + sum(a - 1)
+
+
+try:
+    a = int(input('Введите натуральное число а: '))
+    b = int(input('Введите натуральное число b: '))
+    print(f'{a} + {b} = {sum(a) + sum(b)}')
+except Exception:
+    print('Вы ввели не натуральное число!')
+
+print(timeit("sum(a)", setup="from __main__ import sum, a", number=100000))
