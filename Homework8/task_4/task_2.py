7a61ad90e8f22bd0b35b715e7adf1abcf98a5e96
Dusha <nadezdafurman84@gmail.com>
2023-03-15 17:23:21 +0600

    Modified task_4\task_2

--- Homework8/task_4/task_2.py
+++ Homework8/task_4/task_2.py
@@ -3,12 +3,12 @@
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
 
-my_file = open('file_2.txt')
+my_file = open('file_2.txt',encoding='utf-8')
 content = my_file.read()
 print(f'Содержимое файла:')
 print(content)
 
-my_file = open('file_2.txt')
+my_file = open('file_2.txt',encoding='utf-8')
 content = my_file.readlines()
 print(f'Количество строк в файле: {len(content)}')
 for string in content:
