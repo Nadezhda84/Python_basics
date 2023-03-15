360d16c574e37e6b45a6835eb7fb0bdf0d49bf3f
Dusha <nadezdafurman84@gmail.com>
2023-03-15 14:15:10 +0600

    Added solution task_4\task_1

--- Homework8/task_4/task_1.py
+++ Homework8/task_4/task_1.py
@@ -2,3 +2,15 @@
 Создать программно файл в текстовом формате, записать в него построчно данные,
 вводимые пользователем. Об окончании ввода данных свидетельствует пустая строка.
 """
+my_list = []
+while True:
+    string = input("Введите строку (пустая строка - окончание ввода): ")
+    if string == '':
+        print(my_list)
+        exit()
+    else:
+        new_string = string + '\n'
+        my_list.append(new_string)
+
+    with open("test_1.txt", "w") as file_obj:
+        file_obj.writelines(my_list)
