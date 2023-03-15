7d34328ed51f16faaebdea3542b68db7f931c532
Dusha <nadezdafurman84@gmail.com>
2023-03-15 15:19:04 +0600

    Added solution task_4\task_2

--- Homework8/task_4/task_2.py
+++ Homework8/task_4/task_2.py
@@ -2,3 +2,15 @@
 Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+my_file = open('file_2.txt')
+content = my_file.read()
+print(f'Содержимое файла:')
+print(content)
+
+my_file = open('file_2.txt')
+content = my_file.readlines()
+print(f'Количество строк в файле: {len(content)}')
+for string in content:
+    print(f'Количество слов в строке: {len(string.split())}')
+my_file.close()
