12a1931d767293297af71be3f4d17da5e7688c34
Dusha <nadezdafurman84@gmail.com>
2023-03-16 13:44:33 +0600

    Added solution task_2

--- Homework8/task_2/main.py
+++ Homework8/task_2/main.py
@@ -23,3 +23,39 @@
 и достучаться до списка, который и нужно пополнять
 а потом сохранять все в файл
 """
+import os
+import json
+
+current_dir = os.getcwd()
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    filename = os.path.join(current_dir, 'orders.json')
+
+    if os.path.exists(filename):
+        data = {}
+
+        with open(filename, encoding="utf-8") as fl:
+            data = json.loads(fl.read())
+
+        data['orders'].append(
+            {'item': item, 'quantity': quantity, 'price': price,
+             'buyer': buyer, 'date': date})
+
+        with open(filename, "w", encoding="utf-8") as fl:
+            json.dump(data, fl, indent=4, separators=(',', ': '),
+                      ensure_ascii=False)
+
+        print(f'Данные добавлены в {filename}')
+
+    else:
+        print(f'Исходный файл по пути {filename} не найден')
+
+
+if __name__ == '__main__':
+    write_order_to_json('компьютер', '2', '210000', 'Ivanov I.I.',
+                        '15.03.2023')
+    write_order_to_json('компьютер', '1', '100000', 'Petrov P.P.',
+                        '30.12.2022')
+    write_order_to_json('клавиатура', '1', '3000', 'Petrov P.P.',
+                        '30.12.2022')
