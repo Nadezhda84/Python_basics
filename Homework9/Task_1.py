6ab6b82b57a13908b9701ecf64bef48ba90300e6
Dusha <nadezdafurman84@gmail.com>
2023-03-21 18:08:02 +0600

    Modified Task 1

--- Homework9/Task_1.py
+++ Homework9/Task_1.py
@@ -53,9 +53,9 @@ class Position(Worker):
 
 
 worker_1 = Worker(5, 'Иванов', 'Тестировщик', 40000, 15000)
-worker_2 = Worker('Петр', 'Петров', 'Разработчик', 140000, 30000)
-print(Worker.__str__(worker_1))
-print(Worker.__str__(worker_2))
+# worker_2 = Worker('Петр', 'Петров', 6 , 140000, 30000)
+print(Worker(worker_1))
+print(Worker(worker_2))
 print(Position.get_full_name(worker_1))
 print(Position.get_total_income(worker_1))
 print(Position.get_full_name(worker_2))
