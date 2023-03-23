39527783c39a1264a40b393fc4c8dc03ad573eaf
Dusha <nadezdafurman84@gmail.com>
2023-03-23 23:39:46 +0600

    Modified task 3

--- Homework10/task_3.py
+++ Homework10/task_3.py
@@ -10,25 +10,12 @@
 придумайте как это сделать
 """
 
-import re
-
-
-def has_cyrillic(text):
-    return bool(re.search('[а-яА-Я]', text))
-
-
 my_list = ['attribute', 'класс', 'функция', 'type']
 my_list_bytes = []
-my_list_error = []
 for line in my_list:
-    if has_cyrillic(line) == True:
-        my_list_error.append(line)
-    else:
-        my_list_bytes.append(bytes(line, 'utf-8'))
-
-print(
-    f"Данные слова можно записать в байтовом типе с помощью маркировки b'': "
-    f"{my_list_bytes}")
-print(
-    f"Данные слова невозможно записать в байтовом типе с помощью маркировки b'': "
-    f"{my_list_error}")
+    try:
+        my_list_bytes.append(bytes(line, 'ascii'))
+    except Exception:
+        print(f"«{line}» невозможно записать в байтовом типе с помощью "
+              f"маркировки b''")
+print(my_list_bytes)
