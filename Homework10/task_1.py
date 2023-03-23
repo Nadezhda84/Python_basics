c38502e8a4cf76f1dc04d52242f1924a5b6cb338
Dusha <nadezdafurman84@gmail.com>
2023-03-23 12:16:32 +0600

    Added solution task 1

--- Homework10/task_1.py
+++ Homework10/task_1.py
@@ -16,3 +16,15 @@
 кодовых точек
 --- используйте списки и циклы, не дублируйте функции
 """
+my_list_letter = ['разработка', 'сокет', 'декоратор']
+for line in my_list_letter:
+    print('Тип переменной: {}\n'.format(type(line)))
+    print('Содержание переменной: {}\n'.format(line))
+
+my_list_unicode = [
+    '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
+    '\u0441\u043e\u043a\u0435\u0442',
+    '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440']
+for line in my_list_unicode:
+    print('Тип переменной: {}\n'.format(type(line)))
+    print('Содержание переменной: {}\n'.format(line))
